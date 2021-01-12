24b6c0e2180a4a91b72ce10205e2b393227b3e7f
Simon Charters <simon259@hotmail.co.uk>
2021-01-12 19:33:11 +0000

    working converter one way

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ document.addEventListener("DOMContentLoaded", () => {
         el: "#app",
         data: {
             currencyData: null,
-            selectedCurrency: null
+            selectedCurrency: null,
+            amount: null,
+            convertedAmount:  null,
         },
         
         mounted: function(){
@@ -16,8 +18,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 fetch('https://api.exchangeratesapi.io/latest')
                 .then( res => res.json())
                 .then(data => this.currencyData = data)
-            },                
-            
+            },
+            convert: function(){
+                const currency = this.currencyData.rates[this.selectedCurrency]
+                this.convertedAmount = this.amount * currency
+            }
         }
     })
 })
